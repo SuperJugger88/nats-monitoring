0f0fe8e2bac4dac6371726f3bb2b46d3c78b2f92
Andrey Ivanov <SuperJugger88@gmail.com>
2024-03-03 00:39:37 +0300

    chor: a few minor changes into README and scripts

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cp .env.example .env
+cp nats-laravel/.env.example nats-laravel/.env
 docker compose up -d --build
 docker exec php-fpm bash -c "/home/php/bin/composer install && php artisan key:generate && php artisan optimize:clear"
